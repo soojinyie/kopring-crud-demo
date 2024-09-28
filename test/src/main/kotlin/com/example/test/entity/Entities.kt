191a491340271246d56489e6f5d801e2f396f019
soojinyie <soojinyie@gmail.com>
2024-09-29 03:39:44 +0900

    from local PostgreSQL to Neon cloud database

--- test/src/main/kotlin/com/example/test/entity/Entities.kt
+++ test/src/main/kotlin/com/example/test/entity/Entities.kt
@@ -2,7 +2,6 @@ package com.example.test.entity
 
 import jakarta.persistence.Entity
 import jakarta.persistence.Id
-import org.springframework.data.annotation.CreatedDate
 
 @Entity
 data class Event (
