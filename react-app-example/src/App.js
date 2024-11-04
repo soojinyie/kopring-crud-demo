ad6718ca1e840da4943a1ac332e945a038153936
soojinyie <soojinyie@gmail.com>
2024-11-04 05:05:53 +0000

    add:modify ui

--- react-app-example/src/App.js
+++ react-app-example/src/App.js
@@ -14,8 +14,6 @@ function App() {
 
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <form onSubmit={handleSubmit}>
           <div>
             <label htmlFor="id">ID:</label>
@@ -37,18 +35,6 @@ function App() {
           </div>
           <button type="submit">Submit</button>
         </form>
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
     </div>
   );
 }
